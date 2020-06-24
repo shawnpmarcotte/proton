bfb341090a139423b64d614393b8f99ab6563f86
josedani08200 <jose.dani08200@gmail.com>
2020-06-24 11:57:20 -0400

    adding photo and updating css

--- pages/donate.js
+++ pages/donate.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import Layout from "../components/Layout";
 import NavOne from "../components/NavOne";
 import PageHeader from "../components/PageHeader";
@@ -6,14 +6,14 @@ import Footer from "../components/Footer";
 import Donate from "../components/Donate";
 
 const DonatePage = () => {
-    return (
-        <Layout pageTitle="Oxpitan | Donate">
-            <NavOne />
-            <PageHeader title="Donate" />
-            <Donate />
-            <Footer />
-        </Layout>
-    );
+  return (
+    <Layout pageTitle="Oxpitan | Donate">
+      <NavOne />
+      <PageHeader title="Donate" />
+      <Donate />
+      <Footer />
+    </Layout>
+  );
 };
 
 export default DonatePage;
